fef96c6514684c085b4628124316ebc26f461528
Vlad Holubiev <golubev.vld@gmail.com>
2018-03-11 16:02:55 +0200

    feat: implement swapIndices

--- src/handlers/reindex/algolia.js
+++ src/handlers/reindex/algolia.js
@@ -1,11 +1,28 @@
 import {getClient} from './algolia-client';
 
+const MAIN_INDEX_NAME = 'tneu_news';
+const TEMP_INDEX_NAME = 'tneu_news_temp';
+
 export async function getIndex() {
   const client = getClient();
-  return client.initIndex('tneu_news');
+  return client.initIndex(MAIN_INDEX_NAME);
 }
 
 export async function getIndexTemp() {
   const client = getClient();
-  return client.initIndex('tneu_news_temp');
+  return client.initIndex(TEMP_INDEX_NAME);
+}
+
+export async function swapIndices() {
+  const client = getClient();
+  const index = await getIndex();
+  const indexTemp = await getIndexTemp();
+  console.log(`[swap][indices-init]`);
+
+  const indexSettings = await index.getSettings();
+  await indexTemp.setSettings(indexSettings);
+  console.log(`[swap][settings-copied]`);
+
+  await client.moveIndex(TEMP_INDEX_NAME, MAIN_INDEX_NAME);
+  console.log(`[swap][moved]`);
 }
--- src/handlers/reindex/algolia.test.js
+++ src/handlers/reindex/algolia.test.js
@@ -1,15 +1,25 @@
 jest.mock('./algolia-client');
 
 import {getClient} from './algolia-client';
-import {getIndexTemp, getIndex} from './algolia';
+import {getIndexTemp, getIndex, swapIndices} from './algolia';
 
-const initIndexMock = jest.fn();
+const moveIndexMock = jest.fn();
+const getSettingsMock = jest.fn();
+const setSettingsMock = jest.fn();
+const initIndexMock = jest.fn().mockReturnValue({
+  getSettings: getSettingsMock,
+  setSettings: setSettingsMock
+});
 getClient.mockReturnValue({
-  initIndex: initIndexMock
+  initIndex: initIndexMock,
+  moveIndex: moveIndexMock
 });
 
 beforeEach(() => {
   initIndexMock.mockClear();
+  getSettingsMock.mockClear();
+  setSettingsMock.mockClear();
+  moveIndexMock.mockClear();
 });
 
 describe('#getIndex', () => {
@@ -31,5 +41,28 @@ describe('#getIndexTemp', () => {
   it('should call initIndex w/ temp name', async () => {
     await getIndexTemp();
     expect(initIndexMock).toBeCalledWith('tneu_news_temp');
+  });
+});
+
+describe('#swapIndices', () => {
+  it('should export swapIndices function', () => {
+    expect(swapIndices).toBeInstanceOf(Function);
+  });
+
+  it('should call getSettings', async () => {
+    await swapIndices();
+    expect(getSettingsMock).toBeCalledWith();
+  });
+
+  it('should call setSettings w/ old index settings', async () => {
+    getSettingsMock.mockReturnValueOnce({a: 1});
+    await swapIndices();
+
+    expect(setSettingsMock).toBeCalledWith({a: 1});
+  });
+
+  it('should call moveIndex w/ names - from old to new', async () => {
+    await swapIndices();
+    expect(moveIndexMock).toBeCalledWith('tneu_news_temp', 'tneu_news');
   });
 });
