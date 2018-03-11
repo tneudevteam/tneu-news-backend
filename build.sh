e31804d5d707b491bba47bfce4a0c7e1ef004055
Vlad Holubiev <golubev.vld@gmail.com>
2018-03-11 17:41:12 +0200

    chore: install deps from .build

--- build.sh
+++ build.sh
@@ -10,5 +10,7 @@ cp package.json .build/package.json
 cp .yarnclean .build/.yarnclean
 cp yarn.lock .build/yarn.lock
 
-yarn --modules-folder=.build/node_modules --production
-du -sh .build/node_modules
+cd .build
+yarn --production
+du -sh ./node_modules
+cd ..
